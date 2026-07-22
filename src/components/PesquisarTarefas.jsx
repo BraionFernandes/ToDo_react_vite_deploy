ceb1315de01e8a91443ec2f51747e5497a35b701
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2026-07-21 21:23:57 -0300

    ToDo - update

--- src/components/PesquisarTarefas.jsx
+++
@@ -1,9 +0,0 @@
-import React,{useState} from 'react'
-
-export default function PesquisarTarefas(props){
-    return(
-        <>
-            <input type="search" className='search--input' name="search" id='search' placeholder='Pesquisar...' value={props.search} onInput={(e)=>props.setSearch(e.target.value)}/>
-        </>
-    )
-}
